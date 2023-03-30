3cb0c9b75382eca725c33f5961d65a0a30dc2724
supra <rikc.2000@gmail.com>
2023-03-30 11:31:03 -0400

    floyd warshall

--- graph/floydWarshall.cpp
+++ graph/floydWarshall.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 #define V 6 //No of vertices
 
-void floyd_warshall(int graph[V][V])
-{
+void floyd_warshall(int graph[V][V]) {
     int dist[V][V];
 
     //Assign all values of graph to allPairs_SP
@@ -15,8 +14,7 @@ void floyd_warshall(int graph[V][V])
     //Find all pairs shortest path by trying all possible paths
     for (int k = 0; k < V; ++k)         //Try all intermediate nodes
         for (int i = 0; i < V; ++i)     //Try for all possible starting position
-            for (int j = 0; j < V; ++j) //Try for all possible ending position
-            {
+            for (int j = 0; j < V; ++j) {//Try for all possible ending position
                 if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX) //SKIP if K is unreachable from i or j is unreachable from k
                     continue;
                 else if (dist[i][k] + dist[k][j] < dist[i][j]) //Check if new distance is shorter via vertex K
@@ -25,24 +23,21 @@ void floyd_warshall(int graph[V][V])
 
     //Check for negative edge weight cycle
     for (int i = 0; i < V; ++i)
-        if (dist[i][i] < 0)
-        {
+        if (dist[i][i] < 0) {
             cout << "Negative edge weight cycle is present\n";
             return;
         }
 
     //Print Shortest Path Graph
     //(Values printed as INT_MAX defines there is no path)
-    for (int i = 1; i < V; ++i)
-    {
+    for (int i = 1; i < V; ++i) {
         for (int j = 0; j < V; ++j)
             cout << i << " to " << j << " distance is " << dist[i][j] << "\n";
         cout << "=================================\n";
     }
 }
 
-int main()
-{
+int main() {
     int graph[V][V] = {{0, 1, 4, INT_MAX, INT_MAX, INT_MAX},
                        {INT_MAX, 0, 4, 2, 7, INT_MAX},
                        {INT_MAX, INT_MAX, 0, 3, 4, INT_MAX},
